6f2a247f5ad5cf25f04ef591a160d9ecee27ce97
yzy98 <yz186337@163.com>
2026-02-28 15:05:00 +0800

    refactor: update database client creation to use a factory function

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,7 @@
-import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma as db };
+export const createDbClient = (databaseUrl: string) => {
+  const adapter = new PrismaPg({ connectionString: databaseUrl });
+  return new PrismaClient({ adapter });
+};
