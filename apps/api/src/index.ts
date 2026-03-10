09205125aa9c925cb297b5a93b4fb18838dcba9c
yzy98 <yz186337@163.com>
2026-03-10 14:01:19 +0800

    feat: disable both graphiql and introspection in production

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,4 @@
+import { useDisableIntrospection } from "@graphql-yoga/plugin-disable-introspection";
 import { usePersistedOperations } from "@graphql-yoga/plugin-persisted-operations";
 import { createDbClient } from "@packages/db";
 import persistedOperations from "@packages/graphql-artifacts/persisted-formatted-manifests";
@@ -7,24 +8,30 @@ import { schema } from "@/graphql/schema";
 export interface Env {
   DATABASE_URL: string;
   CORS_ORIGIN: string;
+  ENVIRONMENT: "development" | "production";
 }
 
 const yoga = createYoga<Env>({
   schema,
-  cors: (_request, env) => ({
-    origin: (env as Env).CORS_ORIGIN,
-    methods: ["GET", "POST"],
-  }),
   context: (ctx) => {
     return {
       db: createDbClient(ctx.DATABASE_URL),
     };
   },
+  cors: (_req, ctx) => ({
+    origin: (ctx as Env).CORS_ORIGIN,
+    methods: ["GET", "POST"],
+  }),
+  graphiql: (_req, ctx) => ctx.ENVIRONMENT === "development",
   plugins: [
     usePersistedOperations({
       getPersistedOperation(key: string) {
         return (persistedOperations as Record<string, string>)[key] ?? null;
       },
+    }),
+    useDisableIntrospection({
+      isDisabled: (_req, ctx) =>
+        (ctx as unknown as Env).ENVIRONMENT === "production",
     }),
   ],
 });
